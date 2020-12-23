f4fc41a7c92fbb1601b6babcf981e60a9d906c1f
hispace <hispace.j@gmail.com>
2020-12-23 21:17:04 +0900

    user view page

--- ewha-farm/src/views/apps/user-edit/UserEditAccount.vue
+++ ewha-farm/src/views/apps/user-edit/UserEditAccount.vue
@@ -3,34 +3,45 @@
     <!-- Content Row -->
     <div class="vx-row">
       <div class="vx-col md:w-1/2 w-full">
-        <vs-input class="w-full mt-4" disabled label="이름" v-model="data_local.username" v-validate="'required|alpha_num'" name="username" />
-        <span class="text-danger text-sm"  v-show="errors.has('username')">{{ errors.first('username') }}</span>
 
-        <vs-input class="w-full mt-4" disabled label="핸드폰번호" v-model="data_local.mobile" type="mobile_phone" v-validate="'required|numeric'" name="email" />
-        <span class="text-danger text-sm"  v-show="errors.has('mobile_phone')">{{ errors.first('mobile_phone') }}</span>
+        <vs-input class="w-full mt-4" label="이름" v-model="data_local.name" v-validate="'required|alpha_num'"
+          name="name" />
+        <span class="text-danger text-sm" v-show="errors.has('name')">{{ errors.first('name') }}</span>
+        
+        <vs-input class="w-full mt-4" label="핸드폰번호" v-model="data_local.mobile" type="mobile_phone"
+          v-validate="'required|numeric'" name="email" />
+        <span class="text-danger text-sm" v-show="errors.has('mobile_phone')">{{ errors.first('mobile_phone') }}</span>
 
-        <vs-input class="w-full mt-4" disabled label="농장 주소" v-model="data_local.name" v-validate="'required|alpha_spaces'" name="name" />
-        <span class="text-danger text-sm"  v-show="errors.has('name')">{{ errors.first('name') }}</span>
+        <div class="mt-4">
+          <label class="vs-input--label">계정 상태</label>
+          <v-select v-model="status_local" :clearable="false" :options="statusOptions" v-validate="'required'"
+            name="status" :dir="$vs.rtl ? 'rtl' : 'ltr'" />
+          <span class="text-danger text-sm" v-show="errors.has('status')">{{ errors.first('status') }}</span>
+        </div>
       </div>
 
       <div class="vx-col md:w-1/2 w-full">
-        <div class="mt-4">
-          <label class="vs-input--label">계정 상태</label>
-          <v-select v-model="status_local" :clearable="false" :options="statusOptions" v-validate="'required'" name="status" :dir="$vs.rtl ? 'rtl' : 'ltr'" />
-          <span class="text-danger text-sm"  v-show="errors.has('status')">{{ errors.first('status') }}</span>
-        </div>
+
+        <vs-input class="w-full mt-4" disabled label="아이디" v-model="data_local.username" v-validate="'required|alpha_num'"
+          name="username" />
+        <span class="text-danger text-sm" v-show="errors.has('username')">{{ errors.first('username') }}</span>
+
+        <vs-input class="w-full mt-4" label="농장 주소" v-model="data_local.country" v-validate="'required|alpha_spaces'"
+          name="country" />
+        <span class="text-danger text-sm" v-show="errors.has('country')">{{ errors.first('country') }}</span>
 
         <div class="mt-4">
           <label class="vs-input--label">권한</label>
-          <v-select v-model="role_local" :clearable="false" :options="roleOptions" v-validate="'required'" name="role" :dir="$vs.rtl ? 'rtl' : 'ltr'" />
-          <span class="text-danger text-sm"  v-show="errors.has('role')">{{ errors.first('role') }}</span>
+          <v-select v-model="role_local" :clearable="false" :options="roleOptions" v-validate="'required'" name="role"
+            :dir="$vs.rtl ? 'rtl' : 'ltr'" />
+          <span class="text-danger text-sm" v-show="errors.has('role')">{{ errors.first('role') }}</span>
         </div>
       </div>
 
       <div class="vx-col w-full mt-6">
         <vs-textarea label="메모" v-model="data_local.memo" />
       </div>
-      
+
     </div>
 
     <!-- Permissions -->
@@ -54,7 +65,8 @@
               our data structure. You just have to loop over above variable to get table headers.
               Below we made it simple. So, everyone can understand.
              -->
-            <th class="font-semibold text-base text-left px-3 py-2" v-for="heading in ['Module', 'Read', 'Write', 'Create', 'Delete']" :key="heading">{{ heading }}</th>
+            <th class="font-semibold text-base text-left px-3 py-2"
+              v-for="heading in ['Module', 'Read', 'Write', 'Create', 'Delete']" :key="heading">{{ heading }}</th>
           </tr>
 
           <tr v-for="(val, name) in data_local.permissions" :key="name">
@@ -81,77 +93,98 @@
 </template>
 
 <script>
-import vSelect from 'vue-select'
+  import vSelect from 'vue-select'
 
-export default {
-  components: {
-    vSelect
-  },
-  props: {
-    data: {
-      type: Object,
-      required: true
-    }
-  },
-  data () {
-    return {
-
-      data_local: JSON.parse(JSON.stringify(this.data)),
-
-      statusOptions: [
-        { label: 'Active',  value: 'active' },
-        { label: 'Blocked',  value: 'blocked' },
-        { label: 'Deactivated',  value: 'deactivated' }
-      ],
-      roleOptions: [
-        { label: 'Admin',  value: 'admin' },
-        { label: 'User',  value: 'user' },
-        { label: 'Staff',  value: 'staff' }
-      ]
-    }
-  },
-  computed: {
-    status_local: {
-      get () {
-        return { label: this.capitalize(this.data_local.status),  value: this.data_local.status  }
-      },
-      set (obj) {
-        this.data_local.status = obj.value
+  export default {
+    components: {
+      vSelect
+    },
+    props: {
+      data: {
+        type: Object,
+        required: true
       }
     },
-    role_local: {
-      get () {
-        return { label: this.capitalize(this.data_local.role),  value: this.data_local.role  }
-      },
-      set (obj) {
-        this.data_local.role = obj.value
+    data() {
+      return {
+        data_local: JSON.parse(JSON.stringify(this.data)),
+
+        statusOptions: [{
+            label: 'Active',
+            value: 'active'
+          },
+          {
+            label: 'Blocked',
+            value: 'blocked'
+          },
+          {
+            label: 'Deactivated',
+            value: 'deactivated'
+          }
+        ],
+        roleOptions: [{
+            label: 'Admin',
+            value: 'admin'
+          },
+          {
+            label: 'User',
+            value: 'user'
+          },
+          {
+            label: 'Staff',
+            value: 'staff'
+          }
+        ]
       }
     },
-    validateForm () {
-      return !this.errors.any()
-    }
-  },
-  methods: {
-    capitalize (str) {
-      return str.slice(0, 1).toUpperCase() + str.slice(1, str.length)
+    computed: {
+      status_local: {
+        get() {
+          return {
+            label: this.capitalize(this.data_local.status),
+            value: this.data_local.status
+          }
+        },
+        set(obj) {
+          this.data_local.status = obj.value
+        }
+      },
+      role_local: {
+        get() {
+          return {
+            label: this.capitalize(this.data_local.role),
+            value: this.data_local.role
+          }
+        },
+        set(obj) {
+          this.data_local.role = obj.value
+        }
+      },
+      validateForm() {
+        return !this.errors.any()
+      }
     },
-    save_changes () {
-      /* eslint-disable */
-      if (!this.validateForm) return
+    methods: {
+      capitalize(str) {
+        return str.slice(0, 1).toUpperCase() + str.slice(1, str.length)
+      },
+      save_changes() {
+        /* eslint-disable */
+        if (!this.validateForm) return
 
-      // Here will go your API call for updating data
-      // You can get data in "this.data_local"
+        // Here will go your API call for updating data
+        // You can get data in "this.data_local"
 
-      /* eslint-enable */
-    },
-    reset_data () {
-      this.data_local = JSON.parse(JSON.stringify(this.data))
-    },
-    update_avatar () {
-      // You can update avatar Here
-      // For reference you can check dataList example
-      // We haven't integrated it here, because data isn't saved in DB
+        /* eslint-enable */
+      },
+      reset_data() {
+        this.data_local = JSON.parse(JSON.stringify(this.data))
+      },
+      update_avatar() {
+        // You can update avatar Here
+        // For reference you can check dataList example
+        // We haven't integrated it here, because data isn't saved in DB
+      }
     }
   }
-}
 </script>
---
+++ ewha-farm/src/views/apps/user-view/UserView.vue
@@ -0,0 +1,271 @@
+<template>
+    <div id="page-user-view">
+
+        <vs-alert color="danger" title="User Not Found" :active.sync="user_not_found">
+            <span>User record with id: {{ $route.params.userId }} not found. </span>
+            <span>
+                <span>Check </span>
+                <router-link :to="{name:'page-user-list'}" class="text-inherit underline">All Users</router-link>
+            </span>
+        </vs-alert>
+
+        <div id="user-data" v-if="user_data">
+            <vx-card title="Account" class="mb-base">
+                <div class="vx-row">
+                    <!-- Information - Col 1 -->
+                    <div class="vx-col flex-1" id="account-info-col-1">
+                        <table>
+                            <tr>
+                                <td class="font-semibold">이름</td>
+                                <td>{{ user_data.name }}</td>
+                            </tr>
+                            <tr>
+                                <td class="font-semibold">핸드폰번호</td>
+                                <td>{{ user_data.mobile }}</td>
+                            </tr>
+                            <tr>
+                                <td class="font-semibold">계정 상태</td>
+                                <td>{{ user_data.status }}</td>
+                            </tr>
+                        </table>
+                    </div>
+                    <!-- /Information - Col 1 -->
+
+                    <!-- Information - Col 2 -->
+                    <div class="vx-col flex-1" id="account-info-col-2">
+                        <table>
+                            <tr>
+                                <td class="font-semibold">아이디</td>
+                                <td>{{ user_data.username }}</td>
+                            </tr>
+                            <tr>
+                                <td class="font-semibold">농장 주소</td>
+                                <td>{{ user_data.name }}</td>
+                            </tr>
+                            <tr>
+                                <td class="font-semibold">권한</td>
+                                <td>{{ user_data.role }}</td>
+                            </tr>
+                        </table>
+                    </div>
+
+                </div>
+
+                <vx-card class="mt-4" no-shadow card-border>                    
+                    <span> {{user_data.memo}} </span>
+                </vx-card>
+
+                <div class="vx-row w-full flex ml-auto mt-6" id="account-manage-buttons">
+                    <vs-button icon-pack="feather" icon="icon-edit" class="mr-4"
+                        :to="{name: 'user-edit', params: { userId: $route.params.userId }}">Edit</vs-button>
+                    <vs-button type="border" color="danger" icon-pack="feather" icon="icon-trash"
+                        @click="confirmDeleteRecord">Delete</vs-button>
+                </div>
+
+            </vx-card>
+
+            <div class="vx-row">
+                <div class="vx-col lg:w-1/2 w-full">
+                    <vx-card title="축사" class="mb-base">
+                        <table>
+                            <tr>
+                                <td class="font-semibold">Birth Date</td>
+                                <td>{{ user_data.dob }}</td>
+                            </tr>
+                            <tr>
+                                <td class="font-semibold">Mobile</td>
+                                <td>{{ user_data.mobile }}</td>
+                            </tr>
+                            <tr>
+                                <td class="font-semibold">Website</td>
+                                <td>{{ user_data.website }}</td>
+                            </tr>
+                            <tr>
+                                <td class="font-semibold">Languages</td>
+                                <td>{{ user_data.languages_known.join(", ") }}</td>
+                            </tr>
+                            <tr>
+                                <td class="font-semibold">Gender</td>
+                                <td>{{ user_data.gender }}</td>
+                            </tr>
+                            <tr>
+                                <td class="font-semibold">Contact</td>
+                                <td>{{ user_data.contact_options.join(", ") }}</td>
+                            </tr>
+                        </table>
+                    </vx-card>
+                </div>
+
+                <div class="vx-col lg:w-1/2 w-full">
+                    <vx-card title="함체" class="mb-base">
+                        <table>
+                            <tr>
+                                <td class="font-semibold">Twitter</td>
+                                <td>{{ user_data.social_links.twitter }}</td>
+                            </tr>
+                            <tr>
+                                <td class="font-semibold">Facebook</td>
+                                <td>{{ user_data.social_links.facebook }}</td>
+                            </tr>
+                            <tr>
+                                <td class="font-semibold">Instagram</td>
+                                <td>{{ user_data.social_links.instagram }}</td>
+                            </tr>
+                            <tr>
+                                <td class="font-semibold">Github</td>
+                                <td>{{ user_data.social_links.github }}</td>
+                            </tr>
+                            <tr>
+                                <td class="font-semibold">CodePen</td>
+                                <td>{{ user_data.social_links.codepen }}</td>
+                            </tr>
+                            <tr>
+                                <td class="font-semibold">Slack</td>
+                                <td>{{ user_data.social_links.slack }}</td>
+                            </tr>
+                        </table>
+                    </vx-card>
+                </div>
+            </div>
+
+            <!-- Permissions -->
+            <vx-card>
+
+                <div class="vx-row">
+                    <div class="vx-col w-full">
+                        <div class="flex items-end px-3">
+                            <feather-icon svgClasses="w-6 h-6" icon="LockIcon" class="mr-2" />
+                            <span class="font-medium text-lg leading-none">Permissions</span>
+                        </div>
+                        <vs-divider />
+                    </div>
+                </div>
+
+                <div class="block overflow-x-auto">
+                    <table class="w-full permissions-table">
+                        <tr>
+                            <!--
+                You can also use `Object.keys(Object.values(data_local.permissions)[0])` this logic if you consider,
+                our data structure. You just have to loop over above variable to get table headers.
+                Below we made it simple. So, everyone can understand.
+               -->
+                            <th class="font-semibold text-base text-left px-3 py-2"
+                                v-for="heading in ['Module', 'Read', 'Write', 'Create', 'Delete']" :key="heading">
+                                {{ heading }}</th>
+                        </tr>
+
+                        <tr v-for="(val, name) in user_data.permissions" :key="name">
+                            <td class="px-3 py-2">{{ name }}</td>
+                            <td v-for="(permission, name) in val" class="px-3 py-2" :key="name+permission">
+                                <vs-checkbox v-model="val[name]" class="pointer-events-none" />
+                            </td>
+                        </tr>
+                    </table>
+                </div>
+
+            </vx-card>
+        </div>
+    </div>
+</template>
+
+<script>
+    import moduleUserManagement from '@/store/user-management/moduleUserManagement.js'
+    import user_data from './user.json'
+
+    export default {
+        data() {
+            return {
+                user_data: user_data,
+                user_not_found: false
+            }
+        },
+        computed: {
+            userAddress() {
+                let str = ''
+                for (const field in this.user_data.location) {
+                    str += `${field  } `
+                }
+                return str
+            }
+        },
+        methods: {
+            confirmDeleteRecord() {
+                this.$vs.dialog({
+                    type: 'confirm',
+                    color: 'danger',
+                    title: 'Confirm Delete',
+                    text: `You are about to delete "${this.user_data.username}"`,
+                    accept: this.deleteRecord,
+                    acceptText: 'Delete'
+                })
+            },
+            deleteRecord() {
+                this.$router.push({
+                    name: 'app-user-list'
+                })
+                this.showDeleteSuccess()
+            },
+            showDeleteSuccess() {
+                this.$vs.notify({
+                    color: 'success',
+                    title: 'User Deleted',
+                    text: 'The selected user was successfully deleted'
+                })
+            }
+        },
+    }
+</script>
+
+<style lang="scss">
+    #avatar-col {
+        width: 10rem;
+    }
+
+    #page-user-view {
+        table {
+            td {
+                vertical-align: top;
+                min-width: 140px;
+                padding-bottom: .8rem;
+                word-break: break-all;
+            }
+
+            &:not(.permissions-table) {
+                td {
+                    @media screen and (max-width:370px) {
+                        display: block;
+                    }
+                }
+            }
+        }
+    }
+
+    // #account-info-col-1 {
+    //   // flex-grow: 1;
+    //   width: 30rem !important;
+    //   @media screen and (min-width:1200px) {
+    //     & {
+    //       flex-grow: unset !important;
+    //     }
+    //   }
+    // }
+
+
+    @media screen and (min-width:1201px) and (max-width:1211px),
+    only screen and (min-width:636px) and (max-width:991px) {
+        #account-info-col-1 {
+            width: calc(100% - 12rem) !important;
+        }
+
+        // #account-manage-buttons {
+        //   width: 12rem !important;
+        //   flex-direction: column;
+
+        //   > button {
+        //     margin-right: 0 !important;
+        //     margin-bottom: 1rem;
+        //   }
+        // }
+
+    }
+</style>
