d323a5172101f737b1905b1027e4cb0f5c33cd07
hispace <hispace.j@gmail.com>
2021-01-13 00:58:55 +0900

    sensor graph static data

--- ewha-farm/src/views/users/monitoring/SensorMonitor.vue
+++ ewha-farm/src/views/users/monitoring/SensorMonitor.vue
@@ -1,36 +1,39 @@
 <template>
-  <b-row>
-    <b-col
-      lg="6"
-      sm="12"
+  <div>
+    <b-row
+      class="match-height"
+      no-body
     >
-      <sensor-state-card />
-    </b-col>
-    <b-col
-      lg="6"
-      sm="12"
-    >
-      <weather-card />
-    </b-col>
-    <b-col
-      lg="6"
-      sm="12"
-    >
-      <sensor-graph-card
-        title="온도/습도"
-        :data="data1"
-      />
-    </b-col>
-    <b-col
-      lg="6"
-      sm="12"
-    >
-      <sensor-graph-card
-        title="암모니아 / 황화수소"
-        :data="data2"
-      />
-    </b-col>
-  </b-row>
+      <b-col
+        lg="4"
+        sm="12"
+      >
+        <weather-card />
+      </b-col>
+      <b-col
+        lg="8"
+        sm="12"
+      >
+        <sensor-state-card />
+      </b-col>
+      <b-col
+        lg="6"
+        sm="12"
+      >
+        <sensor-graph-card
+          title="온도/습도"
+        />
+      </b-col>
+      <b-col
+        lg="6"
+        sm="12"
+      >
+        <sensor-graph-card
+          title="암모니아 / 황화수소"
+        />
+      </b-col>
+    </b-row>
+  </div>
 </template>
 
 <script>
@@ -38,9 +41,6 @@ import {
   BRow,
   BCol,
 } from 'bootstrap-vue'
-
-import data1 from '@/data/tem-sensor.json'
-import data2 from '@/data/nh3-sensor.json'
 
 import SensorStateCard from './sensor-card/SensorStateCard.vue'
 import WeatherCard from './sensor-card/WeatherCard.vue'
@@ -53,12 +53,6 @@ export default {
     SensorStateCard,
     WeatherCard,
     SensorGraphCard,
-  },
-  data() {
-    return {
-      data1,
-      data2,
-    }
   },
 }
 </script>
--- ewha-farm/src/views/users/monitoring/sensor-card/SensorGraphCard.vue
+++ ewha-farm/src/views/users/monitoring/sensor-card/SensorGraphCard.vue
@@ -1,6 +1,7 @@
 <template>
-  <b-card
+  <b-card-actions
     :title="title"
+    action-collapse
   >
     <b-row>
       <!-- chart -->
@@ -9,19 +10,16 @@
         :data="data"
       />
     </b-row>
-  </b-card>
+  </b-card-actions>
 </template>
 
 <script>
-import {
-  BCard, BRow,
-} from 'bootstrap-vue'
+import BCardActions from '@core/components/b-card-actions/BCardActions.vue'
 import ChartjsComponentLineChart from './charts-components/ChartjsComponentLineChart.vue'
 
 export default {
   components: {
-    BCard,
-    BRow,
+    BCardActions,
     ChartjsComponentLineChart,
   },
   props: {
@@ -29,10 +27,27 @@ export default {
       type: String,
       default: '',
     },
-    data: {
-      type: Object,
-      default: null,
-    },
+  },
+  data() {
+    return {
+      data: {
+        labels: [1500, 1600, 1700, 1750, 1800, 1850, 1900, 1950, 1999, 2050],
+        datasets: [
+          {
+            data: [86, 114, 106, 106, 107, 111, 133, 221, 783, 2478],
+            label: '온도',
+            borderColor: '#7367F0',
+            fill: false,
+          },
+          {
+            data: [282, 350, 411, 502, 635, 809, 947, 1402, 3700, 5267],
+            label: '습도',
+            borderColor: '#28C76F',
+            fill: false,
+          },
+        ],
+      },
+    }
   },
 }
 </script>
