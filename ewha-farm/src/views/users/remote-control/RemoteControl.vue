76821dee0714400054656a8bc3841c2b4134242f
hispace <hispace.j@gmail.com>
2021-01-17 15:52:48 +0900

    control card component for device

--- ewha-farm/src/views/users/remote-control/RemoteControl.vue
+++ ewha-farm/src/views/users/remote-control/RemoteControl.vue
@@ -1,64 +1,23 @@
 <template>
-  <b-card-actions
-    :title="title"
-    action-collapse
-  >
-    <b-row>
-
-      <!-- range -->
-      <b-col md="6">
-        <b-form-group>
-          <h5>Range</h5>
-          <feather-icon
-            icon="CalendarIcon"
-            size="16"
-          />
-          <flat-pickr
-            v-model="rangeDate"
-            class="form-control"
-            :config="{ mode: 'range'}"
-          />
-        </b-form-group>
-      </b-col>
-
-    </b-row>
-  </b-card-actions>
+  <control-card title="test" />
 </template>
 
 <script>
-import {
-  BRow,
-  BCol,
-  BFormGroup,
-} from 'bootstrap-vue'
-import BCardActions from '@core/components/b-card-actions/BCardActions.vue'
-import flatPickr from 'vue-flatpickr-component'
+
+import ControlCard from './components/ControlCard.vue'
 
 export default {
   components: {
-    BRow,
-    BCardActions,
-    BCol,
-
-    flatPickr,
-    BFormGroup,
+    ControlCard,
   },
   data() {
     return {
-      date: null,
-      dateDefault: null,
-      timePicker: null,
-      dateNtim: null,
-      multiDate: null,
-      rangeDate: null,
-      humanDate: null,
-      disableDate: null,
-      inlineDate: null,
+
     }
   },
 }
 </script>
 
-<style lang="scss">
-@import '@core/scss/vue/libs/vue-flatpicker.scss';
+<style>
+
 </style>
---
+++ ewha-farm/src/views/users/remote-control/components/ControlCard.vue
@@ -0,0 +1,103 @@
+<template>
+  <b-card no-body>
+    <b-card-header>
+      <b-card-title> {{ title }} </b-card-title>
+      <div class="d-flex justify-content-start">
+        <feather-icon
+          icon="EditIcon"
+          size="18"
+          class="text-muted cursor-pointer m-1"
+          @click="modifyNameModal = true"
+        />
+        <feather-icon
+          v-model="isAlert"
+          :icon="alertIcon"
+          size="18"
+          class="text-muted cursor-pointer m-1"
+          @click="alertSetting"
+        />
+        <feather-icon
+          icon="SettingsIcon"
+          size="18"
+          class="text-muted cursor-pointer m-1"
+        />
+      </div>
+    </b-card-header>
+    <b-card-body>
+      <span> Hello </span>
+    </b-card-body>
+
+    <!-- modal -->
+    <b-modal
+      v-model="modifyNameModal"
+      title="변경할 이름을 입력해주세요"
+      ok-title="수정"
+      cancel-variant="outline-secondary"
+      centered
+      @ok="modifyName"
+    >
+      <form
+        ref="form"
+      >
+        <b-form-group
+          label="장치 이름"
+          label-for="name-input"
+        >
+          <b-form-input
+            id="name-input"
+            v-model="name"
+            required
+          />
+        </b-form-group>
+      </form>
+    </b-modal>
+  </b-card>
+</template>
+
+<script>
+import {
+  BCard, BCardHeader, BCardTitle, BCardBody, BModal, BFormInput, BFormGroup,
+} from 'bootstrap-vue'
+
+export default {
+  components: {
+    BCard,
+    BCardHeader,
+    BCardTitle,
+    BCardBody,
+    BModal,
+    BFormGroup,
+    BFormInput,
+  },
+  props: {
+    title: {
+      type: String,
+      default: '',
+    },
+  },
+  data() {
+    return {
+      modifyNameModal: false,
+      isAlert: false,
+      alertIcon: 'BellIcon',
+      name: this.title,
+    }
+  },
+  computed: {
+
+  },
+  watch: {
+    isAlert() {
+      this.alertIcon = this.isAlert ? 'BellIcon' : 'BellOffIcon'
+    },
+  },
+  methods: {
+    modifyName() {
+      console.log(this.name)
+    },
+    alertSetting() {
+      this.isAlert = !this.isAlert
+    },
+  },
+}
+</script>
